b5b89171ee03388b9f91954dd0ab341fb03b726a
vcmc <evan.burke+git@gmail.com>
2016-05-16 22:48:09 -0700

    add brightness adjust capability; fix bug with blank hours at 12pm in 12hr mode, etc.

--- rtc_plus_display_poc.ino
+++ rtc_plus_display_poc.ino
@@ -1,6 +1,5 @@
 // sourced from the Adafruit RTC clock example but modified slightly by vcmc
 
-
 // Clock example using a seven segment display & DS1307 real-time clock.
 //
 // Must have the Adafruit RTClib library installed too!  See:
@@ -55,10 +54,100 @@ long timeSinceLastUpdate = 5000;
 
 // Remember if the colon was drawn on the display so it can be blinked
 // on and off every second.
+// initial value:
 bool blinkColon = true;
 
-// display brightness, from 0 to 15:
-const int displayBrightness = 5;
+// display brightness, from 0 to 15: (initial)
+int displayBrightness = 4;
+
+// input pin for light sensor used for display dimming:
+int inputPin = A1;
+
+//manageBrightness function values
+int brightVal = 0; // variable used for checking brightness
+const int numReadings = 6;    // how many readings to average, one per second
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int br_total = 0;                  // the running total
+int average = 0;                // the average
+
+
+// -------------------------------------------
+
+void printTime() {
+      // Print out the time to serial for debug purposes:
+      DateTime now = rtc.now();
+      Serial.print("Read date & time from DS1307: ");
+      Serial.print(now.year(), DEC);
+      Serial.print('/');
+      Serial.print(now.month(), DEC);
+      Serial.print('/');
+      Serial.print(now.day(), DEC);
+      Serial.print(' ');
+      Serial.print(now.hour(), DEC);
+      Serial.print(':');
+      Serial.print(now.minute(), DEC);
+      Serial.print(':');
+      Serial.print(now.second(), DEC);
+      Serial.println();
+}
+
+// -------------------------------------------
+
+void manageBrightness() {
+  brightVal = 1023 - analogRead(inputPin);
+  // This is set up in the circuit so that max brightness brings the input pin low; low brightness brings it high.
+  // 10k resistor from vcc to analog in & 1 pin of photoresistor; other pin of photores to gnd
+  // We subtract the read value from 1023 to get a number whigh increases with higher brightness. 
+
+// probably need to build some kind of gamma map here? maybe. unless the photoresistor is already on a log scale. 
+
+// also smoothing function to prevent oscillation? smarter than this maybe:
+
+// basic smoothing:
+  // subtract the last reading:
+  br_total = br_total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = brightVal;
+  // add the reading to the total:
+  br_total = br_total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = (br_total / numReadings);
+
+  // while 0-15 is the range of the 7 segment display, in practice higher values are far too bright, so limit this to a smaller range.
+  // 0-10 is probably decent
+  int bright4b = map(average, 0, 1023, 0, 13);
+
+  // we tend to be too sensitive at the low end of the range, so dial it back here
+  if ( bright4b >= 2 ) { 
+    bright4b = bright4b - 2; 
+  } else {
+    bright4b = 0; 
+  }
+
+  // debug: 
+  if ( (seconds % 20) == 0 && Serial ) {
+    Serial.print("brightness input avg: "); Serial.print(average); 
+    Serial.print("  cur bright: ");   Serial.print(brightVal);
+    Serial.print("  mapped to 0-15: "); Serial.println(bright4b);
+    delay(100);
+  }
+
+  clockDisplay.setBrightness(bright4b);
+
+}
+
+
+// -------------------------------------------
 
 void setup() {
   // Setup function runs once at startup to initialize the display
@@ -68,6 +157,9 @@ void setup() {
   if (Serial) {
     Serial.begin(9600);
     Serial.println("Clock starting!");
+    Serial.print("compiler current time: ");
+    Serial.print(__DATE__); Serial.print(" ");
+    Serial.println(__TIME__);
   }
 
   // Setup the display.
@@ -75,7 +167,7 @@ void setup() {
   clockDisplay.setBrightness(displayBrightness);
   clockDisplay.print(10000); // displays "- - - -" due to value out of range
   clockDisplay.writeDisplay();
-  delay(1000);
+  delay(100);
 
   // Setup the DS1307 real-time clock.
   rtc.begin();
@@ -97,8 +189,20 @@ void setup() {
     // Alternatively you can set the RTC with an explicit date & time, 
     // for example to set January 21, 2014 at 3am you would uncomment:
     //rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    Serial.print("Time set to: ");
+    printTime();
+    Serial.println();
   }
-}
+
+// used in manageBrightness:
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+  
+} // end setup
+
+// -------------------------------------------
 
 void loop() {
   // Loop function runs over and over again to implement the clock logic.
@@ -128,21 +232,9 @@ void loop() {
     // Get the time from the DS1307.
     DateTime now = rtc.now();
 
-    if (Serial) {
+    if (Serial) { 
       // Print out the time for debug purposes:
-      Serial.print("Read date & time from DS1307: ");
-      Serial.print(now.year(), DEC);
-      Serial.print('/');
-      Serial.print(now.month(), DEC);
-      Serial.print('/');
-      Serial.print(now.day(), DEC);
-      Serial.print(' ');
-      Serial.print(now.hour(), DEC);
-      Serial.print(':');
-      Serial.print(now.minute(), DEC);
-      Serial.print(':');
-      Serial.print(now.second(), DEC);
-      Serial.println();
+      printTime();
     }
     // Now set the hours and minutes.
     hours = now.hour();
@@ -160,10 +252,10 @@ void loop() {
   // Do 24 hour to 12 hour format conversion when required.
   if (!TIME_24_HOUR) {
     // Handle when hours are past 12 by subtracting 12 hours (1200 value).
-    if (hours > 11) {
+    if (hours > 12) {
       displayValue -= 1200;
     }
-    // Handle hour 0 (midnight) being shown as 12.
+    // Hour 0 (midnight) should be shown as 12.
     else if (hours == 0) {
       displayValue += 1200;
     }
@@ -171,23 +263,20 @@ void loop() {
 
   // Now print the time value to the display.
   clockDisplay.print(displayValue, DEC);
-
-  // Add zero padding when in 24 hour mode and it's midnight.
-  // In this case the print function above won't have leading 0's
-  // which can look confusing.  Go in and explicitly add these zeros.
-  if (TIME_24_HOUR && hours == 0) {
-    // Pad hour 0.
-    clockDisplay.writeDigitNum(1, 0);
-    // Also pad when the 10's minute is 0 and should be padded.
-    if (minutes < 10) {
-      clockDisplay.writeDigitNum(2, 0);
-    }
+  
+  // Also pad when the 10's minute is 0 and should be padded.
+  if (minutes < 10) {
+    // digit 2 on the display is the colon; 3 is actually the 3rd digit
+    clockDisplay.writeDigitNum(3, 0);
   }
 
   // Blink the colon by flipping its value every loop iteration
   // (which happens every second).
   blinkColon = !blinkColon;
   clockDisplay.drawColon(blinkColon);
+
+  // Adjust brightness if needed. You should probably comment this out if you're not using it, or brightness may get weird due to floating analog pin. 
+  manageBrightness();
 
   // Now push out to the display the new values that were set above.
   clockDisplay.writeDisplay();
@@ -219,5 +308,6 @@ void loop() {
     }
   }
 
-  // end loop: 
+  // Loop code is finished, it will jump back to the start of the loop
+  // function again!
  }
